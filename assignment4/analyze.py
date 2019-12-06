d3b4e90b99b3908dbe784e8dcfeeda043f8c1f58
firatciftci <ciftcifirat1@gmail.com>
2019-12-05 20:21:43 -0600

    Finished assignment 4

--- assignment4/analyze.py
+++ assignment4/analyze.py
@@ -1,6 +1,31 @@
 from core import *
 import datetime
 
+JACCARD_THRESHOLD = 0.50
+
+
+def tokenize(st):
+    '''
+    Takes in a string, converts all characters to lowercase, and tokenizes
+    by splitting each individual word
+
+    Returns a new set of tokenized string
+    '''
+    return set(st.lower().split())
+
+
+def jaccard(a, b):
+    '''
+    Given two strings, computes the Jaccard similarity score between them
+
+    Returns the Jaccard similarity score
+    '''
+    Ta = tokenize(a)
+    Tb = tokenize(b)
+
+    return len(Ta.intersection(Tb)) / len(Ta.union(Tb))
+
+
 def match():
     '''
     Match must return a list of tuples of amazon ids and google ids.
@@ -8,16 +33,23 @@ def match():
     [('b000jz4hqo', http://www.google.com/base/feeds/snippets/11125907881740407428'),....]
 
     '''
+    matches = []
+    amazon = amazon_catalog()
+    google = google_catalog()
 
-    #YOUR CODE GOES HERE
+    for i in amazon:
+        for j in google:
+            if jaccard(i['title'], j['title']) >= JACCARD_THRESHOLD:
+                matches.append((i['id'], j['id']))
 
-    return []
+    return matches
 
-#prints out the accuracy
+
+# prints out the accuracy
 now = datetime.datetime.now()
 out = eval_matching(match())
-timing = (datetime.datetime.now()-now).total_seconds()
+timing = (datetime.datetime.now() - now).total_seconds()
 print("----Accuracy----")
 print(out)
 print("---- Timing ----")
-print(timing,"seconds")
+print(timing, "seconds")
